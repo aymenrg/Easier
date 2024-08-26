5073b2aec8a5929b418024a3bf8af87efd52c0a6
Aymen Rguieg <Aymenrg2019@gmail.com>
2024-08-26 22:02:08 +0100

    Resolved merge conflicts

--- Desktop/Easier/geeksforgeeks/settings.py
+++ Desktop/Easier/geeksforgeeks/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 from . info import *
 import os
@@ -29,7 +28,7 @@ EMAIL_PORT = EMAIL_PORT
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', '4p9@s1#fjw+&!o$g%9!8s7nh(5$*b$3%prg=ei!@hepq3m@dzg')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
