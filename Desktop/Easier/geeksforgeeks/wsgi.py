54219e99197e9b264e442be25723e4b9d3692a62
Aymen Rguieg <ayrg2004@gmail.com>
2024-08-28 19:38:35 +0100

    upgrade

--- Desktop/Easier/geeksforgeeks/wsgi.py
+++ Desktop/Easier/geeksforgeeks/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Easier.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EASIER.settings')
 
 application = get_wsgi_application()
